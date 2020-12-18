ad54524f0761c92857e96d9a6f0b973f0c5efa3f
manlio <manliot@uninorte.edu.co>
2020-12-17 23:29:37 -0500

    initial config

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+
+export default App;
