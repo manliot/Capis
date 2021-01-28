5ebf01160a327d143b5e83f3c5f47ebdc1836e9e
manlio <manliot@uninorte.edu.co>
2021-01-28 00:25:30 -0500

    Mockups

---
+++ firebase/Services/Auth.js
@@ -0,0 +1,17 @@
+/* import { auth, provider } from '../firebase'
+
+export const SingInWithGoogle = () => {
+    let user;
+    try {
+        auth.signInWithEmailAndPassword('manliotejeda@gmail.com', '123698745')
+            .then(res => {
+                console.log(res)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    } catch (e) {
+        console.log(e)
+    }
+    return user
+} */
---
+++ firebase/firebase.js
@@ -0,0 +1,27 @@
+/* import React from 'react'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC1bLnADtlRPWjhRG4YSKFIsZJ6npDpNVU",
+    authDomain: "capis-b180f.firebaseapp.com",
+    projectId: "capis-b180f",
+    storageBucket: "capis-b180f.appspot.com",
+    messagingSenderId: "33058736268",
+    appId: "1:33058736268:web:644e3ce2be0565a38a53a9",
+    measurementId: "G-YGHHN94G0P"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+let db = null
+
+if (!firebase.apps.length) {
+    db = firebase.initializeApp(firebaseConfig);
+}
+
+
+
+
+export { db } */
